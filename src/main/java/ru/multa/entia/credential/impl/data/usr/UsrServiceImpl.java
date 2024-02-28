ebbf006dd3af90119732486a0c118b6e3e65cb46
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-02-28 22:42:47 +0300

    UsrServiceImpl is done

--- src/main/java/ru/multa/entia/credential/impl/data/usr/UsrServiceImpl.java
+++ src/main/java/ru/multa/entia/credential/impl/data/usr/UsrServiceImpl.java
@@ -1,52 +1,116 @@
 package ru.multa.entia.credential.impl.data.usr;
 
-import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.multa.entia.credential.api.data.usr.Usr;
+import ru.multa.entia.credential.api.data.usr.UsrEntity;
 import ru.multa.entia.credential.api.data.usr.UsrRepo;
 import ru.multa.entia.credential.api.data.usr.UsrService;
+import ru.multa.entia.results.api.repository.CodeRepository;
 import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Service
-@RequiredArgsConstructor
 public class UsrServiceImpl implements UsrService {
+    public enum Code {
+        ENTITY_BY_ID_ABSENCE,
+        ENTITIES_BY_FIRST_NAME_ABSENCE,
+        ENTITIES_BY_PATER_NAME_ABSENCE,
+        ENTITIES_BY_SURNAME_ABSENCE,
+        ENTITIES_BY_EMAIL_ABSENCE,
+        FAIL_SAVING
+    }
+
+    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
+    static {
+        CR.update(Code.ENTITY_BY_ID_ABSENCE, "credential:service.usr:entity-by-id-is-absence");
+        CR.update(Code.ENTITIES_BY_FIRST_NAME_ABSENCE, "credential:service.usr:entities-by-first-name-is-absence");
+        CR.update(Code.ENTITIES_BY_PATER_NAME_ABSENCE, "credential:service.usr:entities-by-pater-name-is-absence");
+        CR.update(Code.ENTITIES_BY_SURNAME_ABSENCE, "credential:service.usr:entities-by-surname-is-absence");
+        CR.update(Code.ENTITIES_BY_EMAIL_ABSENCE, "credential:service.usr:entities-by-email-is-absence");
+        CR.update(Code.FAIL_SAVING, "credential:service.usr:fail-saving");
+    }
+
     private final UsrRepo repo;
+    private final Function<Usr, UsrEntity> toEntityConverter;
+    private final Function<UsrEntity, Usr> toUsrConverter;
+
+    @Autowired
+    public UsrServiceImpl(final UsrRepo repo,
+                          final Function<Usr, UsrEntity> toEntityConverter,
+                          final Function<UsrEntity, Usr> toUsrConverter) {
+        this.repo = repo;
+        this.toEntityConverter = toEntityConverter;
+        this.toUsrConverter = toUsrConverter;
+    }
 
     @Override
     public Result<Usr> getById(final ObjectId id) {
-        return null;
+        Optional<UsrEntity> maybeEntity = repo.findById(id);
+        return maybeEntity.isPresent()
+                ? DefaultResultBuilder.<Usr>ok(toUsrConverter.apply(maybeEntity.get()))
+                : DefaultResultBuilder.<Usr>fail(CR.get(Code.ENTITY_BY_ID_ABSENCE));
     }
 
     @Override
     public Result<Usr> save(final Usr user) {
-        return null;
+        try {
+            UsrEntity saved = repo.save(toEntityConverter.apply(user));
+            return DefaultResultBuilder.<Usr>ok(toUsrConverter.apply(saved));
+        } catch (Throwable ex) {
+            return DefaultResultBuilder.<Usr>fail(CR.get(Code.FAIL_SAVING));
+        }
     }
 
     @Override
     public Result<List<Usr>> findByFirstName(final String firstName) {
-        return null;
+        List<UsrEntity> entities = repo.findByFirstName(firstName);
+        return entities.isEmpty()
+                ? createFailResult(Code.ENTITIES_BY_FIRST_NAME_ABSENCE)
+                : createOkResult(entities);
     }
 
     @Override
     public Result<List<Usr>> findByPaterName(final String paterName) {
-        return null;
+        List<UsrEntity> entities = repo.findByPaterName(paterName);
+        return entities.isEmpty()
+                ? createFailResult(Code.ENTITIES_BY_PATER_NAME_ABSENCE)
+                : createOkResult(entities);
     }
 
     @Override
     public Result<List<Usr>> findBySurname(final String surname) {
-        return null;
+        List<UsrEntity> entities = repo.findBySurname(surname);
+        return entities.isEmpty()
+                ? createFailResult(Code.ENTITIES_BY_SURNAME_ABSENCE)
+                : createOkResult(entities);
     }
 
     @Override
     public Result<List<Usr>> findByEmail(final String email) {
-        return null;
+        List<UsrEntity> entities = repo.findByEmail(email);
+        return entities.isEmpty()
+                ? createFailResult(Code.ENTITIES_BY_EMAIL_ABSENCE)
+                : createOkResult(entities);
     }
 
     @Override
     public Result<Usr> deleteById(final ObjectId id) {
-        return null;
+        repo.deleteById(id);
+        return DefaultResultBuilder.<Usr>ok();
+    }
+
+    private Result<List<Usr>> createFailResult(final Code code) {
+        return DefaultResultBuilder.<List<Usr>>fail(CR.get(code));
+    }
+
+    private Result<List<Usr>> createOkResult(final List<UsrEntity> entities) {
+        return  DefaultResultBuilder.<List<Usr>>ok(entities.stream().map(toUsrConverter).toList());
     }
 }
--- src/test/java/ru/multa/entia/credential/impl/data/usr/UsrServiceImplTest.java
+++ src/test/java/ru/multa/entia/credential/impl/data/usr/UsrServiceImplTest.java
@@ -1,48 +1,459 @@
 package ru.multa.entia.credential.impl.data.usr;
 
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import ru.multa.entia.credential.api.data.usr.Usr;
+import ru.multa.entia.credential.api.data.usr.UsrEntity;
 import ru.multa.entia.credential.api.data.usr.UsrRepo;
+import ru.multa.entia.credential.impl.converter.usr.DefaultEntity2UsrConverter;
+import ru.multa.entia.credential.impl.converter.usr.DefaultUsr2EntityConverter;
+import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.results.api.repository.CodeRepository;
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.utils.Results;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
-// TODO: impl
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class UsrServiceImplTest {
+
+    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
+    private static final Function<Usr, UsrEntity> TO_ENTITY_CONVERTER = new DefaultUsr2EntityConverter();
+    private static final Function<UsrEntity, Usr> TO_USR_CONVERTER = new DefaultEntity2UsrConverter();
+
     @Test
-    void shouldCheckByIdGetting() {
+    void shouldCheckByIdGetting_ifEntityAbsence() {
         Supplier<UsrRepo> repoSupplier = () -> {
             UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findById(Mockito.any())).thenReturn(Optional.empty());
+
             return repo;
         };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<Usr> result = service.getById(new ObjectId());
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(UsrServiceImpl.Code.ENTITY_BY_ID_ABSENCE))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckByIdGetting() {
+        UsrEntityImpl entity = new UsrEntityImpl(
+                new ObjectId(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random()
+        );
+
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findById(Mockito.any())).thenReturn(Optional.of(entity));
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<Usr> result = service.getById(new ObjectId());
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+
+        Usr gotterUsr = result.value();
+        assertThat(gotterUsr.id()).isEqualTo(entity.getId());
+        assertThat(gotterUsr.firstName()).isEqualTo(entity.getFirstName());
+        assertThat(gotterUsr.paterName()).isEqualTo(entity.getPaterName());
+        assertThat(gotterUsr.surname()).isEqualTo(entity.getSurname());
+        assertThat(gotterUsr.email()).isEqualTo(entity.getEmail());
     }
 
     @Test
     void shouldCheckSaving() {
+        ObjectId expectedId = new ObjectId();
+        UsrImpl expectedUsr = new UsrImpl(
+                null,
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random()
+        );
 
+        UsrEntityImpl entity = new UsrEntityImpl(
+                null,
+                expectedUsr.firstName(),
+                expectedUsr.paterName(),
+                expectedUsr.surname(),
+                expectedUsr.email()
+        );
+
+        Function<UsrEntity, UsrRepo> repoFunction = e -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            e.setId(expectedId);
+            Mockito.when(repo.save(Mockito.any())).thenReturn(e);
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoFunction.apply(entity), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<Usr> result = service.save(expectedUsr);
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+
+        Usr gotterUsr = result.value();
+        assertThat(gotterUsr.id()).isEqualTo(expectedId);
+        assertThat(gotterUsr.firstName()).isEqualTo(expectedUsr.firstName());
+        assertThat(gotterUsr.paterName()).isEqualTo(expectedUsr.paterName());
+        assertThat(gotterUsr.surname()).isEqualTo(expectedUsr.surname());
+        assertThat(gotterUsr.email()).isEqualTo(expectedUsr.email());
+    }
+
+    @Test
+    void shouldCheckSaving_idFail() {
+        UsrImpl expectedUsr = new UsrImpl(
+                null,
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random()
+        );
+
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.save(Mockito.any())).thenThrow(RuntimeException.class);
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<Usr> result = service.save(expectedUsr);
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(UsrServiceImpl.Code.FAIL_SAVING))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckByFirstNameGetting_ifAbsence() {
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findByFirstName(Mockito.any())).thenReturn(List.of());
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findByFirstName(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(UsrServiceImpl.Code.ENTITIES_BY_FIRST_NAME_ABSENCE))
+                        .back()
+                        .compare()
+        ).isTrue();
     }
 
     @Test
     void shouldCheckByFirstNameGetting() {
+        UsrImpl expectedUsr = new UsrImpl(
+                new ObjectId(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random()
+        );
 
+        UsrEntityImpl expectedEntity = new UsrEntityImpl(
+                expectedUsr.id(),
+                expectedUsr.firstName(),
+                expectedUsr.paterName(),
+                expectedUsr.surname(),
+                expectedUsr.email()
+        );
+
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findByFirstName(Mockito.any())).thenReturn(List.of(expectedEntity));
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findByFirstName(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+
+        List<Usr> gottenUsers = result.value();
+        assertThat(gottenUsers).hasSize(1);
+        assertThat(gottenUsers.get(0)).isEqualTo(expectedUsr);
     }
+
+    @Test
+    void shouldCheckByPaterNameGetting_idAbsence() {
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findByPaterName(Mockito.any())).thenReturn(List.of());
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findByPaterName(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(UsrServiceImpl.Code.ENTITIES_BY_PATER_NAME_ABSENCE))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
 
     @Test
     void shouldCheckByPaterNameGetting() {
+        UsrImpl expectedUsr = new UsrImpl(
+                new ObjectId(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random()
+        );
 
+        UsrEntityImpl expectedEntity = new UsrEntityImpl(
+                expectedUsr.id(),
+                expectedUsr.firstName(),
+                expectedUsr.paterName(),
+                expectedUsr.surname(),
+                expectedUsr.email()
+        );
+
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findByPaterName(Mockito.any())).thenReturn(List.of(expectedEntity));
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findByPaterName(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+
+        List<Usr> gottenUsers = result.value();
+        assertThat(gottenUsers).hasSize(1);
+        assertThat(gottenUsers.get(0)).isEqualTo(expectedUsr);
     }
 
     @Test
-    void shouldCheckBySurnameGetting() {
+    void shouldCheckBySurnameGetting_idAbsence() {
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findBySurname(Mockito.any())).thenReturn(List.of());
 
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findBySurname(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(UsrServiceImpl.Code.ENTITIES_BY_SURNAME_ABSENCE))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+
+    @Test
+    void shouldCheckBySurnameGetting() {
+        UsrImpl expectedUsr = new UsrImpl(
+                new ObjectId(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random()
+        );
+
+        UsrEntityImpl expectedEntity = new UsrEntityImpl(
+                expectedUsr.id(),
+                expectedUsr.firstName(),
+                expectedUsr.paterName(),
+                expectedUsr.surname(),
+                expectedUsr.email()
+        );
+
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findBySurname(Mockito.any())).thenReturn(List.of(expectedEntity));
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findBySurname(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+
+        List<Usr> gottenUsers = result.value();
+        assertThat(gottenUsers).hasSize(1);
+        assertThat(gottenUsers.get(0)).isEqualTo(expectedUsr);
+    }
+
+    @Test
+    void shouldCheckByEmailGetting_ifAbsence() {
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findByEmail(Mockito.any())).thenReturn(List.of());
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findByEmail(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(UsrServiceImpl.Code.ENTITIES_BY_EMAIL_ABSENCE))
+                        .back()
+                        .compare()
+        ).isTrue();
     }
 
     @Test
     void shouldCheckByEmailGetting() {
+        UsrImpl expectedUsr = new UsrImpl(
+                new ObjectId(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random(),
+                Faker.str_().random()
+        );
 
+        UsrEntityImpl expectedEntity = new UsrEntityImpl(
+                expectedUsr.id(),
+                expectedUsr.firstName(),
+                expectedUsr.paterName(),
+                expectedUsr.surname(),
+                expectedUsr.email()
+        );
+
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito.when(repo.findByEmail(Mockito.any())).thenReturn(List.of(expectedEntity));
+
+            return repo;
+        };
+
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        Result<List<Usr>> result = service.findByEmail(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+
+        List<Usr> gottenUsers = result.value();
+        assertThat(gottenUsers).hasSize(1);
+        assertThat(gottenUsers.get(0)).isEqualTo(expectedUsr);
     }
 
     @Test
     void shouldCheckByIdDeleting() {
+        AtomicReference<ObjectId> holder = new AtomicReference<>();
+        Supplier<UsrRepo> repoSupplier = () -> {
+            UsrRepo repo = Mockito.mock(UsrRepo.class);
+            Mockito
+                    .doAnswer(new Answer<Void>() {
+                        @Override
+                        public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
+                            holder.set(invocationOnMock.getArgument(0));
+                            return null;
+                        }
+                    })
+                    .when(repo)
+                    .deleteById(Mockito.any());
 
+            return repo;
+        };
+
+        ObjectId expectedId = new ObjectId();
+        UsrServiceImpl service = new UsrServiceImpl(repoSupplier.get(), TO_ENTITY_CONVERTER, TO_USR_CONVERTER);
+        service.deleteById(expectedId);
+
+        assertThat(holder.get()).isEqualTo(expectedId);
     }
 }
