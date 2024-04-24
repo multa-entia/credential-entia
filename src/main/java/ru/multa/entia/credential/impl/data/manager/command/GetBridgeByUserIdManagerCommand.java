de3933bace14a966d45a963fdad7b85682da1c24
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-04-24 22:22:18 +0300

    GetBridgeByUserIdManagerCommand is done

---
+++ src/main/java/ru/multa/entia/credential/impl/data/manager/command/GetBridgeByUserIdManagerCommand.java
@@ -0,0 +1,51 @@
+package ru.multa.entia.credential.impl.data.manager.command;
+
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import ru.multa.entia.credential.api.data.bridge.Bridge;
+import ru.multa.entia.credential.api.data.bridge.BridgeService;
+import ru.multa.entia.credential.api.data.manager.item.ManagerDatum;
+import ru.multa.entia.credential.impl.data.manager.item.DefaultManagerDatum;
+import ru.multa.entia.results.api.repository.CodeRepository;
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class GetBridgeByUserIdManagerCommand extends AbstractManagerCommand{
+    public static final String PROPERTY_BRIDGES = "bridges";
+
+    public enum Code {
+        SERVICE_IS_ABSENCE
+    }
+
+    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
+    static {
+        CR.update(Code.SERVICE_IS_ABSENCE, "credential:command.get-bridge-by-user-id:service-is-absence");
+    }
+
+    private final ObjectId objectId;
+
+    @Setter
+    private BridgeService bridgeService;
+
+    public GetBridgeByUserIdManagerCommand(final Consumer<Result<ManagerDatum>> consumer,
+                                           final ObjectId objectId) {
+        super(consumer);
+        this.objectId = objectId;
+    }
+
+    @Override
+    protected Result<ManagerDatum> executeService() {
+        if (bridgeService == null) {
+            return DefaultResultBuilder.<ManagerDatum>fail(CR.get(Code.SERVICE_IS_ABSENCE));
+        }
+
+        Result<List<Bridge>> result = bridgeService.getByUserId(objectId);
+        return result.ok()
+                ? DefaultResultBuilder.<ManagerDatum>ok(DefaultManagerDatum.builder().property(PROPERTY_BRIDGES, result.value()).build())
+                : DefaultResultBuilder.<ManagerDatum>fail(result.seed());
+    }
+}
